379bde0acbec8c8e5be5ef09a66de49d7564df71
yuriysurzhikov <yuriisurzhykov@gmail.com>
2022-05-31 14:35:12 +0300

    17 - converted java to kotlin

--- uiComponents/src/main/java/com/cybertaxi/uicomponents/views/SpaceItemDecoration.kt
+++ uiComponents/src/main/java/com/cybertaxi/uicomponents/views/SpaceItemDecoration.kt
@@ -1,31 +1,24 @@
-package com.cybertaxi.uicomponents.views;
+package com.cybertaxi.uicomponents.views
 
-import android.graphics.Rect;
-import android.view.View;
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 
-import androidx.recyclerview.widget.RecyclerView;
-
-public class SpaceItemDecoration extends RecyclerView.ItemDecoration {
-
-    private final int space;
-
-    public SpaceItemDecoration(int space) {
-        this.space = space;
-    }
-
-    @Override
-    public void getItemOffsets(Rect outRect, View view,
-                               RecyclerView parent, RecyclerView.State state) {
-        outRect.left = space;
-        outRect.right = space;
-        outRect.bottom = space;
+class SpaceItemDecoration(private val space: Int) : ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView, state: RecyclerView.State
+    ) {
+        outRect.left = space
+        outRect.right = space
+        outRect.bottom = space
 
         // Add top margin only for the first item to avoid double space between items
         if (parent.getChildLayoutPosition(view) == 0) {
-            outRect.top = space;
+            outRect.top = space
         } else {
-            outRect.top = 0;
+            outRect.top = 0
         }
     }
 }
-
